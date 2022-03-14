3518c7490c4ce4d3b61d777891445470e758e509
Kate Orlova <kate.orlova@unrvld.com>
2022-03-14 19:59:53 +0000

    Defined the default app settings

---
+++ src/BestDeals/Configuration/AppConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+
+namespace BestDeals.Configuration
+{
+    public class AppConfiguration
+    {
+        public string KenticoKontentUrl { get; set; }
+        public DeliveryOptions DefaultDeliveryOptions { get; set; }
+    }
+}
