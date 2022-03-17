991ab3b69f387664f2442ed57f42e3cb64da7fe4
Kate Orlova <kate.orlova@unrvld.com>
2022-03-17 23:18:00 +0000

    Nullable metadata fields

--- src/BestDeals/Models/Interfaces/IMetadata.cs
+++ src/BestDeals/Models/Interfaces/IMetadata.cs
@@ -1,8 +1,10 @@
-﻿namespace BestDeals.Models.Interfaces
+﻿using Kentico.Kontent.Delivery.Abstractions;
+
+namespace BestDeals.Models.Interfaces
 {
     public interface IMetadata
     {
-        string MetadataMetaTitle { get; set; }
-        string MetadataMetaDescription { get; set; }
+        string? MetadataMetaTitle { get; set; }
+        string? MetadataMetaDescription { get; set; }
     }
 }
