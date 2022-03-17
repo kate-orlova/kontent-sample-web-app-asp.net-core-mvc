e022f7b169c53178b795ea3981c0e1362c6b83c2
Kate Orlova <kate.orlova@unrvld.com>
2022-03-17 23:17:14 +0000

    Added IDeal interface

---
+++ src/BestDeals/Models/Interfaces/IDeal.cs
@@ -0,0 +1,9 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+
+namespace BestDeals.Models.Interfaces
+{
+    public interface IDeal
+    {
+        IContentItemSystemAttributes DealSystemAttributes { get; set; }
+    }
+}
