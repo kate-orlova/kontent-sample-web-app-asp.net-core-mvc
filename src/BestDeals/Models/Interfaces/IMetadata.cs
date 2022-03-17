77d9be51a34fb6396ac3d54c22185fb7281162c7
Kate Orlova <kate.orlova@unrvld.com>
2022-03-17 22:28:21 +0000

    Added Metadata interface

---
+++ src/BestDeals/Models/Interfaces/IMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace BestDeals.Models.Interfaces
+{
+    public interface IMetadata
+    {
+        string MetadataMetaTitle { get; set; }
+        string MetadataMetaDescription { get; set; }
+    }
+}
