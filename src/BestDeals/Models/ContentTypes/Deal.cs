213ad07d184a57e83b245b6d8b6eb8512027d011
Kate Orlova <kate.orlova@unrvld.com>
2022-03-17 23:18:20 +0000

    Added Deal class

---
+++ src/BestDeals/Models/ContentTypes/Deal.cs
@@ -0,0 +1,14 @@
+﻿using BestDeals.Models.Interfaces;
+using Kentico.Kontent.Delivery.Abstractions;
+
+namespace BestDeals.Models.ContentTypes
+{
+    public class Deal : IDeal, IMetadata
+    {
+        public IContentItemSystemAttributes DealSystemAttributes { get; set; }
+        public string? MetadataMetaDescription { get; set; }
+        public string? MetadataMetaTitle { get; set; }
+        public string Type => DealSystemAttributes.Type;
+        public string Id => DealSystemAttributes.Id;
+    }
+}
