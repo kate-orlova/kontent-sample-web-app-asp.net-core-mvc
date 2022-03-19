f1ad7410c6e18df24372f06245cd6a42db46a19b
Kate Orlova <kate.orlova@unrvld.com>
2022-03-19 21:30:58 +0000

    Created Deal controller

---
+++ src/BestDeals/Controllers/DealController.cs
@@ -0,0 +1,27 @@
+﻿using BestDeals.Models.ContentTypes;
+using BestDeals.Models.ViewModels;
+using Kentico.Kontent.Delivery.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestDeals.Controllers
+{
+    public class DealController : ControllerBase
+    {
+        public DealController(IDeliveryClientFactory deliveryClientFactory) : base(deliveryClientFactory)
+        {
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var itemsTask = _client.GetItemsAsync<Deal>();
+
+            var model = new DealViewModel
+            {
+                Items = (await itemsTask).Items
+            };
+
+            return View(model);
+        }
+
+    }
+}
