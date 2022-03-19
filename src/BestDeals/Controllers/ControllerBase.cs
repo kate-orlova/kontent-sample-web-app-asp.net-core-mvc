51fa1fa59aa8a7958a2be981329b1768ddc8e1c6
Kate Orlova <kate.orlova@unrvld.com>
2022-03-19 20:38:27 +0000

    Added Base controller

---
+++ src/BestDeals/Controllers/ControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+
+namespace BestDeals.Controllers
+{
+    public class ControllerBase
+    {
+        protected readonly IDeliveryClient _client;
+        public ControllerBase(IDeliveryClientFactory deliveryClientFactory) : this()
+        {
+            _client = deliveryClientFactory.Get();
+        }
+
+        public ControllerBase()
+        {
+
+        }
+    }
+}
