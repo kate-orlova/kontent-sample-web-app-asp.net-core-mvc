b3db2cf8c9af6b6ec5310aaecaffe2c8937b948a
Kate Orlova <kate.orlova@unrvld.com>
2022-03-19 21:44:54 +0000

    Defined AppConfig property

--- src/BestDeals/Infrastructure/SelfConfigActionFilterAttribute.cs
+++ src/BestDeals/Infrastructure/SelfConfigActionFilterAttribute.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using BestDeals.Configuration;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
 
 namespace BestDeals.Infrastructure
 {
     public class SelfConfigActionFilterAttribute : ActionFilterAttribute
     {
+        public IOptionsSnapshot<AppConfiguration> AppConfig { get; }
     }
 }
