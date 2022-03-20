89fe572dfd67ef21519bacb0b112b1fc81c9195e
Kate Orlova <kate.orlova@unrvld.com>
2022-03-20 23:03:00 +0000

    Implemented Redirect helper method

---
+++ src/BestDeals/Helpers/RedirectHelper.cs
@@ -0,0 +1,20 @@
+﻿using BestDeals.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestDeals.Helpers
+{
+    public static class RedirectHelper
+    {
+        public static RedirectToRouteResult GetSelfConfigIndexResult(MessageModel message)
+        {
+            return new RedirectToRouteResult(new RouteValueDictionary(new
+            {
+                Action = "Index",
+                Controller = "SelfConfig",
+                Area = "Admin",
+                MessageBody = message?.Message,
+                message?.MessageType
+            }));
+        }
+    }
+}
