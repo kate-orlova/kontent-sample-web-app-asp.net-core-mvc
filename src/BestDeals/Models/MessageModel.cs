9892cc74afe2e39348d43d7e21faf6988052f0e9
Kate Orlova <kate.orlova@unrvld.com>
2022-03-20 23:02:30 +0000

    Defined Message model

---
+++ src/BestDeals/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BestDeals.Models
+{
+    public class MessageModel
+    {
+        public string Caption { get; set; }
+        public string Message { get; set; }
+        public MessageType MessageType { get; set; }
+    }
+
+    public enum MessageType
+    {
+        Info,
+        Error
+    }
+}
