32c2d2aabe7844a1635a2064a15ae593aac870c2
Kate Orlova <kate.orlova@unrvld.com>
2022-03-22 19:48:15 +0000

    Added a Startup class

---
+++ src/BestDeals/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BestDeals
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
