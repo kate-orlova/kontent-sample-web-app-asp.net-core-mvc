668c6d1a417b3efa2e905dc1eb28f6e65b1d1dd7
Kate Orlova <kate.orlova@unrvld.com>
2022-03-24 01:24:13 +0000

    Defined DealView model

---
+++ src/BestDeals/Models/ViewModels/DealsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using BestDeals.Models.ContentTypes;
+
+namespace BestDeals.Models.ViewModels
+{
+    public class DealsViewModel
+    {
+        public IEnumerable<Deal> Items { get; set; } = new List<Deal>();
+    }
+}
