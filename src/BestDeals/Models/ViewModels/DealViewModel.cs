9961aae326f2855aa1f0a45f89d3baa66ee2775c
Kate Orlova <kate.orlova@unrvld.com>
2022-03-24 01:26:00 +0000

    Deleted unneeded files

--- src/BestDeals/Models/ViewModels/DealViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BestDeals.Models.ContentTypes;
-
-namespace BestDeals.Models.ViewModels
-{
-    public class DealViewModel
-    {
-        public IEnumerable<Deal> Items { get; set; } = new List<Deal>();
-    }
-}
