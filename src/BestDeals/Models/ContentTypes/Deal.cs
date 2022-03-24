313a3e1c48d4fc3cbdd7de784f9981ba2f411d89
Kate Orlova <kate.orlova@unrvld.com>
2022-03-24 01:23:52 +0000

    Declared additional properties for Deal model

--- src/BestDeals/Models/ContentTypes/Deal.cs
+++ src/BestDeals/Models/ContentTypes/Deal.cs
@@ -5,10 +5,18 @@ namespace BestDeals.Models.ContentTypes
 {
     public class Deal : IDeal, IMetadata
     {
+        public const string Codename = "offer";
+        public const string TitleCodename = "title";
+        public const string MarketingTitleCodename = "offer_marketing_details__short_name";
+        public const string MarketingHeadlineCodename = "offer_marketing_details__headline";
+
         public IContentItemSystemAttributes DealSystemAttributes { get; set; }
         public string? MetadataMetaDescription { get; set; }
         public string? MetadataMetaTitle { get; set; }
         public string Type => DealSystemAttributes.Type;
         public string Id => DealSystemAttributes.Id;
+        public string Title { get; set; }
+        public string MarketingTitle { get; set; }
+        public string MarketingHeadline { get; set; }
     }
 }
