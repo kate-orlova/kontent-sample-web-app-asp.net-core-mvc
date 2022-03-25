a62ec339adfa2f6c30f7e33198ea895e1feef52c
Kate Orlova <kate.orlova@unrvld.com>
2022-03-25 22:09:52 +0000

    Implemented Deals controller

---
+++ src/BestDeals/Controllers/DealsController.cs
@@ -0,0 +1,27 @@
+﻿using BestDeals.Models.ContentTypes;
+using BestDeals.Models.ViewModels;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Urls.QueryParameters.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestDeals.Controllers
+{
+    public class DealsController : ControllerBase
+    {
+        public async Task<ActionResult> Index()
+        {
+            var parameters = new List<IQueryParameter>()
+            {
+                new EqualsFilter("system.type", "offer")
+            };
+
+            var response = await _client.GetItemsAsync<Deal>(parameters);
+            var model = new DealsViewModel
+            {
+                Items = response.Items
+            };
+
+            return View(model);
+        }
+    }
+}
